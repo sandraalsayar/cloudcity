c5f310b2743705a2fcb55160da2bfac83262f831
Sandra Alsayar <35713665+sandraalsayar@users.noreply.github.com>
2019-10-18 00:24:16 -0400

    More sheep AI (walk on trigger)
    Now if the player crosses an invisible border, it will trigger sheep walking on waypoints

---
+++ Assets/MazeFlagCollisionCode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeFlagCollisionCode : MonoBehaviour {
+
+	public GameObject box;
+	public Rigidbody player;
+	public Rigidbody sheep;
+	public static bool sheepFlag;
+
+
+	void Start() {
+		
+		box = GetComponent<GameObject>();
+		player = GetComponent<Rigidbody>();
+		sheep = GetComponent<Rigidbody>();
+		sheepFlag = false;
+
+	}
+
+	// make it spin when you get closer to it
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Player")) {
+			sheepFlag = true;
+		}
+	}
+
+}
