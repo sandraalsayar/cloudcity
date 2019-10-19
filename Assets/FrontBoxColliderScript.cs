b38c51e914f0f5a3622a1774568d799d3a6fd23d
Sandra Alsayar <35713665+sandraalsayar@users.noreply.github.com>
2019-10-18 22:19:56 -0400

    Finished sheep AI
    - Sheep now detects player from back: stops and waits, if player stays, it turns to it
    - Sheep now detects player from front: stops completly
    
    New scripts: FrontBoxColliderScript, BehindSphereColliderScript

---
+++ Assets/FrontBoxColliderScript.cs
@@ -0,0 +1,38 @@
+﻿/*
+	FrontBoxColliderScript attached to empty game object of sheep
+	Main author: Sandra
+	Detects player in front.
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontBoxColliderScript : MonoBehaviour
+{
+
+	public static bool FrontCollisionFlag;
+	public static bool StopMoving;
+	public static bool TurnAroundFlag;
+	// public Animator anim;	
+
+
+	void Start() {
+
+		FrontCollisionFlag = false;
+		StopMoving = false;
+
+		// anim = GetComponent<Animator>();
+		// if (anim == null) {
+		// 	Debug.Log("Animator could not be found");
+		// }
+	}
+
+	// Notifies the sheep that the player has enetred from the back
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Player")) {
+			FrontCollisionFlag = true;
+			StopMoving = true;
+
+		}
+	}
+}
