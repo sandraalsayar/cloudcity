e1ef0223417fc9f23dd5579d98290dcf859ed126
yahn41 <yahn41@gatech.edu>
2019-10-20 00:45:25 -0400

    sheep put down item animation
    -added trigger box on bench
    -when star collider collides with trigger box, sheep putting down item animation plays

---
+++ Assets/Scripts/PlaceItemDownTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceItemDownTrigger : MonoBehaviour
+{
+    private Animator anim;
+    private Animator star_anim;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Star"))
+        {
+            anim = other.gameObject.transform.parent.gameObject.GetComponent<Animator>();
+            anim.SetBool("ReachedDestination", true);
+            star_anim = other.gameObject.GetComponent<Animator>();
+            star_anim.SetTrigger("NearBench");
+
+        }
+     
+    }
+}
