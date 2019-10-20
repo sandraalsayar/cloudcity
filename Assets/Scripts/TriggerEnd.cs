981b8c10dc2d94839c87532c38316660a359eaaf
bguerrero6 <biancavguerrero97@gmail.com>
2019-10-19 21:54:29 -0400

    Added End Condition
    - if you collect 1 star and go to home base platform it cues end scene
    - base platform is prefabbed
    - UI from previous commits is a prefab

---
+++ Assets/Scripts/TriggerEnd.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerEnd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    void OnTriggerEnter(Collider c){
+        if (c.attachedRigidbody != null && c.CompareTag("Player") && c.attachedRigidbody.gameObject.GetComponent<StarCollector>().starCount==1)
+        {
+            Debug.Log("endScene");
+            SceneManager.LoadScene("End");
+        }
+    }
+}
