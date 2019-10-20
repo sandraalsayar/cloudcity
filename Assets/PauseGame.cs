65252f8118414640d93da75f3c809df2d9c9a4a1
Sandra Alsayar <35713665+sandraalsayar@users.noreply.github.com>
2019-10-20 14:05:28 -0400

    Restart level when playe is too close

---
+++ Assets/PauseGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class PauseGame : MonoBehaviour
+{
+	private CanvasGroup canvasGroup;
+
+	void Awake() {
+		canvasGroup = GetComponent<CanvasGroup>();
+
+		if (canvasGroup == null) {
+			Debug.LogError("Cannot find component");
+		}
+	}
+
+
+	void Update() {
+
+		if (MazeSheepAI.Lose == true) {
+				canvasGroup.interactable = true;
+				canvasGroup.blocksRaycasts = true;
+				canvasGroup.alpha = 1f;
+				Time.timeScale = 0f;  // this pauses any movement
+		}
+	}
+}
