ec120e721a1df6d9ca903b40e03e70bb9ecded49
yahn41 <yahn41@gatech.edu>
2019-10-24 20:38:45 -0400

    added dancing sheep
    -added dancing sheep and spectators to scene
    -added dancing animations in assets->animations->dancing folder
    -added more models in models folder
    -created script Idle_transition that randomizes which animation to play next in animator for Idle animator controller

---
+++ Assets/Idle_Transition.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idle_Transition : MonoBehaviour
+{
+    private Animator anim;
+    public int random_value;
+    public float waitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        StartCoroutine(WaitUntilAnimEnds());
+     
+    }
+
+    IEnumerator WaitUntilAnimEnds()
+    {
+        //normalizedTime: A value of 1 is the end of the animation. A value of 0.5 is the middle of the animation.
+        while (anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1)
+        {
+            yield return null;
+        }
+     
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Happy Hand Gesture"))
+        {
+            random_value = Random.Range(11, 13);
+            Debug.Log("current state is happy hand gesture");
+        } else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Looking Behind"))
+        {
+            random_value = Random.Range(21, 23);
+        } else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Happy Idle"))
+        {
+            random_value = Random.Range(31, 33);
+        } else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Excited"))
+        {
+            random_value = Random.Range(41, 43);
+        } else
+        {
+            Debug.Log("Cannot find current animator state");
+        }
+
+        anim.SetInteger("random_transition", random_value);
+    }
+}
