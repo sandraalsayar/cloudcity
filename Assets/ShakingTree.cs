045442670c084e7c9986624a271f84df645dafed
yahn41 <yahn41@gatech.edu>
2019-11-08 17:39:40 -0500

    created tree shake animation
    -made tree shake animation
    -placed test tree into cloudmodeltestscene
    -set up a trigger collider box and script so that the player shakes the tree once inside the trigger box
    TO DO:
    -need to write the script
    -need to import the player shaking animation from downloads

---
+++ Assets/ShakingTree.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakingTree : MonoBehaviour
+{
+    private Animation anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim = other.gameObject.GetComponent<Animation>();
+
+        if (anim.isPlaying)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Shaking");
+            anim.Play("Shake");
+        }
+    }
+}
