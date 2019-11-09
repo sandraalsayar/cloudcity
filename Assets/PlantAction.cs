b5c40be77870d5f1d584d1118028c344409a5f16
bguerrero6 <biancavguerrero97@gmail.com>
2019-11-08 22:24:54 -0500

    Plant Model and Animations
    - made plant spot respectfully for plant star in MushiePlant scene
    - created new blendtree for interaction animations of player
    - interactions for (star-player, plant-player) setup
    - plant for star animated
    - star becomes visible and is collected

---
+++ Assets/PlantAction.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantAction : MonoBehaviour
+{
+    private bool isNearPlant;
+    public GameObject plantStar;
+    // Start is called before the first frame update
+    Animator anim;
+    StarCollector starCollector;
+    public GameObject player;
+
+    void Start()
+    {
+        isNearPlant = false;
+        anim = GetComponent<Animator>();
+        starCollector = player.GetComponent<StarCollector>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isNearPlant && starCollector.interacted)
+        {
+            //trigger plant flying animation
+            anim.SetTrigger("isYanked");
+
+            //make star pop out
+            plantStar.SetActive(true);
+
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isNearPlant = true;
+            starCollector.isNearPlant = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isNearPlant = false;
+            starCollector.isNearPlant = false;
+        }
+    }
+
+    //called at end of plant flying animation
+    public void FadePlant(){
+        //TODO: fade away plant and then destroy
+        Destroy(this.gameObject);
+        starCollector.interacted = false;
+        starCollector.isNearPlant = false;
+    }
+}
