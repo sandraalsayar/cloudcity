159f1ced59027a557ab3c1f538a066da9f9ac4ef
Sandra Alsayar <35713665+sandraalsayar@users.noreply.github.com>
2019-11-10 00:29:26 -0500

    Rock and star interaction
    The star doesn't float yet

--- Assets/ActionRock.cs
+++ Assets/ActionRock.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class ActionRock : MonoBehaviour
 {
-	public Rigidbody rock;
-	public Rigidbody player;
-	public Animator anim;	
+	private Rigidbody rock;
+	private Rigidbody player;
+	private Animator anim;	
 	private bool isNearRock;
-	public bool isTurnedOver;
+	public static bool isTurnedOver;
 	// public String openText;
 
 	void Start() {
@@ -24,13 +24,10 @@ public class ActionRock : MonoBehaviour
 	void Update() {
 		// check if player is near rock and rock isn't turned over yet
 		if (isNearRock && isTurnedOver == false) {
-			Debug.Log("Ok i'm isnide!");
 			if(Input.GetKeyDown("x"))
 			{
-				Debug.Log("Player pressed X!");
 				anim.SetTrigger("isTurnedOver");
 				isTurnedOver = true;
-				// animator.SetBool("TurnOver", isTurnedOver);
 			}
 		}
 	}
@@ -38,14 +35,12 @@ public class ActionRock : MonoBehaviour
 	void OnTriggerEnter(Collider other) {
 		if(other.gameObject.CompareTag("Player")) {
 			isNearRock = true;
-			Debug.Log("Player is inside the zone!");
 		}
 	}
 
 	void OnTriggerExit(Collider other) {
 		if(other.gameObject.CompareTag("Player")) {
 			isNearRock = false;
-			Debug.Log("Player is outside the zone!");
 		}
 	}
 	
---
+++ Assets/RockStar_Rend.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockStar_Rend : MonoBehaviour
+{
+    public string name; // name is the index and star number(specific)
+    public Renderer rend;
+
+    void Start() {
+    	rend = GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+
+    void Update() {
+    	// Enable renderer accordingly
+    	if (ActionRock.isTurnedOver) {
+        	rend.enabled = true;
+    	}
+    }
+}
