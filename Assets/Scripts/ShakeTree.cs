e739669f9fdee987c75e0c3f82565e9959441031
yahn41 <yahn41@gatech.edu>
2019-11-15 14:41:34 -0500

    added tree star
    -added tree star and tree shaking animation
    -added tree anim controller, script ShakeTree
    -created star falling animation
    -put tree_star under Stars in hierarchy
    -removed duplicate cloud prefab
    
    *Player can now go to tree and press X to shake tree and have star fall to the ground to collect

---
+++ Assets/Scripts/ShakeTree.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeTree : MonoBehaviour
+{
+    public GameObject treeStar;
+    public GameObject player;
+
+    //reference to star collector class
+    StarCollector starCollector;
+
+    private Animator anim;
+    private Animator treeStarAnim;
+    private bool isNearTree;
+
+    void Start()
+    {
+        isNearTree = false;
+        anim = GetComponent<Animator>();
+        starCollector = player.GetComponent<StarCollector>();
+        treeStarAnim = treeStar.GetComponent<Animator>();
+
+    }
+
+    private void Update()
+    {
+        // if you are near the tree and the PLAYER's animation has played, trigger animation for tree
+        // make star appear
+        if (isNearTree && starCollector.interacted)
+        {
+            //trigger tree shaking animation
+            anim.SetTrigger("isShaken");
+
+
+            //make star fly/fall down
+            treeStar.SetActive(true);
+            treeStarAnim.SetTrigger("TreeIsShaken");
+
+        }
+    }
+    
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                isNearTree = true;
+                starCollector.isNearTree = true;
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                isNearTree = false;
+                starCollector.isNearTree = false;
+            }
+        }
+}
