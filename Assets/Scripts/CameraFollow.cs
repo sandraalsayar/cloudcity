3dc1840851603b1d592af66c2efcdeec33e1382e
bguerrero6 <biancavguerrero97@gmail.com>
2019-11-19 10:50:33 -0500

    Camera Follow
    -added scripts in progress for new camera movement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,26 +4,49 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform cameraTarget;
-    public float sSpeed = 10.0f;
-    public Vector3 dist;
-    public Transform lookTarget;
+    [SerializeField]
+    private float distanceAway;
+    [SerializeField]
+    private float distanceUp;
+    [SerializeField]
+    private float smooth;
+    [SerializeField]
+    private Transform follow;
+    private Vector3 targetPosition;
 
-    void FixedUpdate(){
-        Vector3 dPos = cameraTarget.position + dist;
-        Vector3 sPos = Vector3.Lerp(transform.position, dPos, sSpeed * Time.deltaTime);
-        transform.position = sPos;
-        transform.LookAt(lookTarget.position);
+    void Start(){
+        follow = GameObject.FindWithTag("camera").transform;
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void LateUpdate(){
+        targetPosition = follow.position + follow.up * distanceUp - (follow.forward * distanceAway);
+        //Debug.DrawRay();
+        //Debug.DrawRay();
+        //Debug.Line();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        transform.position = Vector3.Lerp(transform.position,targetPosition,Time.deltaTime*smooth);
+
+        transform.LookAt(follow);
     }
+    //public Transform cameraTarget;
+    //public float sSpeed = 10.0f;
+    //public Vector3 dist;
+    //public Transform lookTarget;
+
+    //void FixedUpdate(){
+    //    Vector3 dPos = cameraTarget.position + dist;
+    //    Vector3 sPos = Vector3.Lerp(transform.position, dPos, sSpeed * Time.deltaTime);
+    //    transform.position = sPos;
+    //    transform.LookAt(lookTarget.position);
+    //}
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
 }
