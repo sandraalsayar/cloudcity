6eec1b6d2612815fe1f1610d85adeead30d13ad7
yahn41 <yahn41@gatech.edu>
2019-12-05 12:49:05 -0500

    day to night transition
    -implemented in DayToNightTransitionScene
    -created day to night prefab

---
+++ Assets/Scripts/ChangeSkyBox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSkyBox : MonoBehaviour
+{
+    public Material skybox;
+    public float step = 0;
+    public Light directionalLight;
+    private Color dayColor;
+    private Color nightColor;
+    private float dayLightIntensity;
+    private float nightLightIntensity;
+    private float duration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dayColor = new Color(0.7137f, 0.7137f, 0.5647f);
+        nightColor = new Color(0.1568f, 0.18039f, 0.2196f);
+        dayLightIntensity = 1.2f;
+        nightLightIntensity = 0.75f;
+        duration = 1200f;
+   
+
+    }
+
+    private void Update()
+    {
+        RenderSettings.skybox.SetColor("_TintColor", Color.Lerp(dayColor, nightColor, step));
+        directionalLight.intensity = Mathf.Lerp(dayLightIntensity, nightLightIntensity, step);
+        DynamicGI.UpdateEnvironment();
+        step += Time.deltaTime / duration;
+
+    }
+
+    void OnDisable()
+    {
+        RenderSettings.skybox.SetColor("_TintColor", dayColor);
+        DynamicGI.UpdateEnvironment();
+    }
+
+}
